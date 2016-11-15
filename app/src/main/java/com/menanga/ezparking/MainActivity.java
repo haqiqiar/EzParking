e2a0806bae345239903935a88e4a81b0f302d8ab
RIZ-PC\haqiqi <rzkhqq@gmail.com>
2016-11-15 13:21:35 +0700

    Initial Commit

---
+++ app/src/main/java/com/menanga/ezparking/MainActivity.java
@@ -0,0 +1,13 @@
+package com.menanga.ezparking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
