49bcfaf3c34798e425df6c1e495fce6c0e433c18
RIZ-PC\haqiqi <rzkhqq@gmail.com>
2016-11-16 15:52:38 +0700

    Migrasi, styling SMainActivity, Manifest default launcher kupindah ke SMainActivity

--- app/src/main/java/com/menanga/ezparking/server/SFindActivity.java
+++ app/src/main/java/com/menanga/ezparking/server/SFindActivity.java
@@ -2,38 +2,26 @@ package com.menanga.ezparking.server;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.menanga.ezparking.R;
-
-import java.util.ArrayList;
 
 public class SFindActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private ArrayList<LatLng> daftar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sfind);
-        daftar = new ArrayList<LatLng>();
-        daftar.add(new LatLng(-7.277950, 112.796932));
-        daftar.add(new LatLng(-7.6, 112.79632));
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
     }
 
 
@@ -49,64 +37,10 @@ public class SFindActivity extends FragmentActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
-            @Override
-            public View getInfoWindow(Marker marker) {
-                return null;
-            }
 
-            @Override
-            public View getInfoContents(Marker marker) {
-                View v = getLayoutInflater().inflate(R.layout.detail_loc, null);
-
-                LatLng latLng = marker.getPosition();
-
-                //TextView tvLat = (TextView) v.findViewById(R.id.mlokasi);
-
-                //tvLat.setText(latLng.latitude + " " + latLng.longitude);
-
-                return v;
-            }
-        });
-
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-
-                //mMap.clear();
-
-                //MarkerOptions mO = new MarkerOptions();
-
-                //mO.position(latLng);
-                if(daftar.contains(latLng)){
-                    mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-                }
-
-
-                //Marker marker = mMap.addMarker(mO);
-
-                //marker.showInfoWindow();
-            }
-        });
-
-
-
-        for(LatLng pos : daftar){
-            mMap.addMarker(new MarkerOptions().position(pos).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(pos,15));
-            // Zoom in, animating the camera.
-            googleMap.animateCamera(CameraUpdateFactory.zoomIn());
-            // Zoom out to zoom level 10, animating with a duration of 2 seconds.
-            googleMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
-        }
         // Add a marker in Sydney and move the camera
-
-        //LatLng ssydney = new LatLng(-34, 152);
-
-
-
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        //mMap.addMarker(new MarkerOptions().position(ssydney).title("Marker in Sydney"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(ssydney));
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
